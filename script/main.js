8b3516b165a20e9bfe5f16d1148835d236b7d8b5
Danilo da Cruz <cruz.delfino@outlook.com>
2024-03-14 13:21:58 -0300

    criado, cadastro, login, submenu em categorias e ajustes de layout

--- script/main.js
+++ script/main.js
@@ -1,40 +1,3 @@
-const swiper = new Swiper('.swiper', {
-	spaceBetween: 20,
- 	slidesPerView: 3,
-	loop: false,
-	pagination: {
-	  el: '.swiper-pagination',
-	},
-  
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-  
-	scrollbar: {
-	  el: '.swiper-scrollbar',
-	},
-  });
-
-const swiperDestaques = new Swiper('.swiperDestaques', {
-	spaceBetween: 20,
- 	slidesPerView: 2,
-	loop: true,
-	pagination: {
-	  el: '.swiper-pagination',
-	},
-  
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-  
-	scrollbar: {
-	  el: '.swiper-scrollbar',
-	},
-  });
-
-
 
 const rodape = document.querySelector('.rodape');
 rodape.innerHTML = `
@@ -97,4 +60,76 @@ rodape.innerHTML = `
 <hr class="rodape__hr"/>
 <p class="rodape__conteudo-texto">&copy 2024 DEEM Apps. Development.</p>`
 
+const swiper = new Swiper('.swiper', {
+	spaceBetween: 20,
+ 	slidesPerView: 3,
+	loop: false,
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+  
+	scrollbar: {
+	  el: '.swiper-scrollbar',
+	},
+  });
 
+const swiperDestaques = new Swiper('.swiperDestaques', {
+	spaceBetween: 20,
+ 	slidesPerView: 2,
+	loop: true,
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+  
+	scrollbar: {
+	  el: '.swiper-scrollbar',
+	},
+  });
+
+const btnSubmenuIndex = document.querySelector('.btn-submenu');
+const btnSubmenuDownload = document.querySelector('.btn-download');
+const btnListaCategoria = document.querySelector('.lista-menu__sublista-categoria');
+const btnListaDownload = document.querySelector('.lista-menu__sublista-download');
+const btnArrowReplaceCategoria = document.querySelector('.arrow-replace-categoria');
+const btnArrowReplaceDownload = document.querySelector('.arrow-replace-download');
+const btnLoginCadastro = document.querySelector('.container__usuario');
+
+btnLoginCadastro.addEventListener('click', () =>{
+
+})
+
+btnSubmenuIndex.addEventListener('click', () => {
+	btnListaCategoria.classList.toggle('hidden');
+	btnArrowReplaceCategoria.classList.toggle('fa-chevron-right');
+	btnArrowReplaceCategoria.classList.toggle('fa-chevron-down');
+})
+btnSubmenuDownload.addEventListener('click', () => {
+	btnListaDownload.classList.toggle('hidden');
+	btnArrowReplaceDownload.classList.toggle('fa-chevron-right');
+	btnArrowReplaceDownload.classList.toggle('fa-chevron-down');
+})
+
+// const conteudoModal = document.querySelector('.modal-content');
+const modalCadastrar = document.querySelector('.modal-cadastrar');
+const modalLogin = document.querySelector('.modal-login');
+const btnLogin = document.querySelector('.btn-login');
+const btnCadastrar = document.querySelector('.btn-cadastrar');
+
+btnLogin.addEventListener('click', () =>{
+	modalCadastrar.classList.toggle('hidden')
+	modalLogin.classList.toggle('hidden')
+})
+btnCadastrar.addEventListener('click', () =>{
+	modalCadastrar.classList.toggle('hidden')
+	modalLogin.classList.toggle('hidden')
+})
